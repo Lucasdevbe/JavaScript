a625213a92cd38260a5099dac973a6361b8a3554
Lucasdev <107226710+Lucasdevbe@users.noreply.github.com>
2022-10-12 22:32:30 -0300

    movendo-pastas

---
+++ exe001/exe001.js
@@ -0,0 +1,13 @@
+
+
+
+var velo = 40
+
+window.alert("digite o valor da velocidade")
+
+
+if (velo <= 50) {
+    console.log("velocidade permitida")
+} else {
+    console.log("multado")
+}
---
+++ exe002/exe002.js
@@ -0,0 +1,9 @@
+
+
+var nac = "portugual".toLowerCase()
+
+if (nac == "brasil") {
+    console.log("voce é brasileiro")
+} else {
+    console.log("você nao é brasileiro")
+}
