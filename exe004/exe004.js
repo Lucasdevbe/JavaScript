e500ffd184742a29aa048c9fd5ba09caef31c7a4
Lucasdev <107226710+Lucasdevbe@users.noreply.github.com>
2024-05-31 21:57:40 -0300

    testando condições II

--- exe004/exe004.js
+++ exe004/exe004.js
@@ -4,17 +4,20 @@ function carregar() {
     var msg = window.document.getElementById('msg')
     var imagem = document.getElementById('imagem')
     var data = new Date()
-    var hora = data.getHours
+    var hora = data.getHours()
 
-    if (hora >= '00:00' && hora <= '12:00') {
+    if (hora >= '00' && hora <= '12') {
         
         msg.innerHTML = 'olá, Bom dia '
 
-    } else if (hora > '12:00' && hora <= '18:00') {
+    } else if (hora > '12' && hora <= '22') {
         
         msg.innerHTML = 'olá, Boa tarde '        
     } else {
-        msg.innerHTML = 'olá, Boa noite'
+
+        msg.innerHTML = `agora sao ${hora} horas`
+        
     }
+
 
 }
