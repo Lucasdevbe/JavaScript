148ad03b64b9c0ac8edd7570501df5ef47cc4494
Lucasdev <107226710+Lucasdevbe@users.noreply.github.com>
2024-06-14 17:56:20 -0300

    apresentador números em uma serie definida

---
+++ exe006/exe007.js
@@ -0,0 +1,12 @@
+function ate10() {
+    
+
+    var msg = window.document.getElementById('msg')
+    
+    for (let index = 0; index < 10; index++) {
+s
+        msg.innerHTML += `numero ${index}`
+
+        
+    }
+}
