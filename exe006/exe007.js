c4733609cfc2f481e66a77bf1d8d2a88c3ffa40d
Lucasdev <107226710+Lucasdevbe@users.noreply.github.com>
2024-06-15 20:34:18 -0300

    Inserindo elementos ao DOM

--- exe006/exe007.js
+++ exe006/exe007.js
@@ -1,11 +1,29 @@
 function ate10() {
     
 
-    var msg = window.document.getElementById('msg')
+    var msg = document.querySelector("#msg")
+    var numero = Number(document.getElementById('numero').value)
+  
+
     
-    for (let index = 0; index < 10; index++) {
-      
-        msg.innerHTML += `numero ${index}</br>`;
+        
+
+    for (let index = 0; index < numero; index++) {
+
+       
+
+        if (index % 5 == 0) {
+    
+            let divn = document.createElement('div')
+            
+
+            msg.appendChild(divn)
+            
+            
+        } 
+
+        
+
     }
    
 }
