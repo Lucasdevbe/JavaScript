44c6b50a9ba7e14c6db422aff77972dcc8e59eeb
Lucasdev <107226710+Lucasdevbe@users.noreply.github.com>
2024-06-15 19:01:43 -0300

    contando até 10

--- exe006/exe007.js
+++ exe006/exe007.js
@@ -4,9 +4,8 @@ function ate10() {
     var msg = window.document.getElementById('msg')
     
     for (let index = 0; index < 10; index++) {
-s
-        msg.innerHTML += `numero ${index}`
-
-        
+      
+        msg.innerHTML += `numero ${index}</br>`;
     }
+   
 }
