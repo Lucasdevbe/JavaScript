2e585dac51c947dd7e9dc73d00c0adee8354a8f3
Lucasdev <107226710+Lucasdevbe@users.noreply.github.com>
2024-06-16 20:10:49 -0300

    Implementado o contador para separar os números

--- exe006/exe007.js
+++ exe006/exe007.js
@@ -1,29 +1,36 @@
 function ate10() {
-    
 
+    // Pegando variavis e campos da pagina
     var msg = document.querySelector("#msg")
     var numero = Number(document.getElementById('numero').value)
-  
-
     
-        
+    // criando contador para o numero escolhido
+    for (let index = 100; index <= numero; index++) {
 
-    for (let index = 0; index < numero; index++) {
 
-       
+        // estabelecendo condição para criar uma div 
+        if (index % 100 == 0) {
 
-        if (index % 5 == 0) {
-    
+            // cria uma div dento da tag pai msg
             let divn = document.createElement('div')
-            
 
+            // inserindo conteudo na div filho q
+            divn.text += `${index}`
+
+            // colocando dentro a tag pai
             msg.appendChild(divn)
-            
-            
-        } 
 
-        
+            // imprimido msg
+            msg.innerHTML += `${index} `
+
+        } else {
+
+            // se não precisar criar uma nova div para querar o conteúdo insere o numero normalmente
+            msg.innerHTML += `${index} `
+        }
+
+
 
     }
-   
+
 }
