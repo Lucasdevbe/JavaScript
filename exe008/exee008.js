e015689598dbef186ade9ec1236d963d7eb009fa
Lucasdev <107226710+Lucasdevbe@users.noreply.github.com>
2024-06-21 20:30:46 -0300

    inserindo valores em array e apresentando  o ultimo inserido

--- exe008/exee008.js
+++ exe008/exee008.js
@@ -1,12 +1,44 @@
+let numero = []
+let msg = window.document.querySelector('#msg')
+
 function finalizar() {
     
+} function limpar(){
+    
+    numero = []
+    msg.innerHTML = ``
+    array.innerHTML = ``
+    
 }
+
 function inserir() {
     
-    let msg = window.document.querySelector('#msg')
-    let numero = [Number(document.getElementById('numero').value)]
+    
+    
+    numero.push(Number(document.getElementById('numero').value))
 
-    msg.innerHTML= `${numero}`
+   
+
+    let i = 0
+    while (i < numero.length) {
+
+        msg.innerHTML = ``
+        let msg_itens = document.createElement('div')
+        msg_itens.Number = numero[i]
+        
+        msg.appendChild(msg_itens)
+
+        msg.innerHTML += `${numero[i]}`
+
+        i++
+    }
+
+    for (let index = 0; index < numero.length; index++) {
+
+        let array = document.getElementById('array')
+         
+        array.innerHTML = `${numero}`
+    }
 
 
 
