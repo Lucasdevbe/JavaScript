6ffe3bf744a59d930f0fe86dd5ae9d2985ba7eb9
Lucasdev <107226710+Lucasdevbe@users.noreply.github.com>
2024-06-21 17:48:02 -0300

    funções

---
+++ exe008/exee008.js
@@ -0,0 +1,15 @@
+function finalizar() {
+    
+}
+function inserir() {
+    
+    let msg = window.document.querySelector('#msg')
+    let numero = [Number(document.getElementById('numero').value)]
+
+    msg.innerHTML= `${numero}`
+
+
+
+    
+
+}
