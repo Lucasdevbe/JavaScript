d250372c0c378939b75a564a93afaabd5ffcafa9
Lucasdev <107226710+Lucasdevbe@users.noreply.github.com>
2024-06-23 22:49:04 -0300

    FUNÇÃO LIMPAR

--- exe008/exee008.js
+++ exe008/exee008.js
@@ -1,34 +1,40 @@
 let numero = []
 let msg = window.document.querySelector('#msg')
 
+
+
 function finalizar() {
-    
-} function limpar(){
-    
+
+} function limpar() {
+
     numero = []
     msg.innerHTML = ``
     array.innerHTML = ``
-    
+
+
 }
 
 function inserir() {
-    
-    
-    
+
+
+
     numero.push(Number(document.getElementById('numero').value))
 
-   
+
 
     let i = 0
+
     while (i < numero.length) {
 
         msg.innerHTML = ``
         let msg_itens = document.createElement('div')
         msg_itens.Number = numero[i]
-        
+
         msg.appendChild(msg_itens)
 
         msg.innerHTML += `${numero[i]}`
+
+
 
         i++
     }
@@ -36,12 +42,9 @@ function inserir() {
     for (let index = 0; index < numero.length; index++) {
 
         let array = document.getElementById('array')
-         
+
         array.innerHTML = `${numero}`
     }
 
-
-
-    
 
 }
